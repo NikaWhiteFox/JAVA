52bbfb519525536866408321d98ae9d09347ecd1
NikaGeek <3ili.8069.mine@gmail.com>
2022-12-01 09:51:41 +0300

    Update Color.java

--- Color.java
+++ Color.java
@@ -4,13 +4,13 @@ class Color {
 	}
 	public static void printColor() {
 	    int value = 100;
-		if (value <=0) {
+		if (value <= 0) {
 		    System.out.println("Red");
 		}
-		if (value>0 && value<=100) {
+		if (value > 0 && value <= 100) {
 		    System.out.println("Yellow");
 		}
-		if (value>100) {
+		if (value > 100) {
 			System.out.println("Green");
 		}
 	}
