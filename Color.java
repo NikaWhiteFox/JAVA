b6066a3fcce82e4ce4415236470f7276e46da9c4
NikaGeek <3ili.8069.mine@gmail.com>
2022-12-01 09:34:40 +0300

    Домашка 1

---
+++ Color.java
@@ -0,0 +1,17 @@
+class Color {
+    public static void main(String[] args) {
+	    printColor();
+	}
+	public static void printColor() {
+	    int value = 100;
+		if (value <=0) {
+		    System.out.println("Red");
+		}
+		if (value>0 && value<=100) {
+		    System.out.println("Yellow");
+		}
+		if (value>100) {
+			System.out.println("Green");
+		}
+	}
+}
