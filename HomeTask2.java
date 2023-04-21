9e102262ffc029f27ecbb4fbe511f9690b202c7d
NikaGeek <3ili.8069.mine@gmail.com>
2023-04-21 14:19:17 +0300

    NewHomeTask2

--- HomeTask2.java
+++ HomeTask2.java
@@ -7,19 +7,19 @@
 
 class HomeTask2 {
     public static void main(String[] args) {
-        System.out.println(checkSumBetween10And20(5, 10) ? "Summ is between 10 and 20 - true." : "Summ is between 10 and 20 - false.");
-        System.out.println(determineNumberPositive(10)? "Number is positive." : "Number is negative.");
-        System.out.println(determineNumberNegative(-8)? "Number is negative - true." : "Number is negative - false.");
+        checkSumBetween10And20(5, 10);
+        determineNumberPositive(10);
+        determineNumberNegative(-8);
         repeatText("Hello! ", 7);
-        determineLeapYear();
+        determineLeapYear(1000);
     }
     
     static boolean checkSumBetween10And20(int a, int b) {
 		return a + b >= 10 && a + b <= 20;
     }
     
-    static boolean determineNumberPositive(int a) {
-        return a >= 0;
+    static void determineNumberPositive(int a) {
+        System.out.println(a >= 0 ? "Number is positive." : "Number is negative.");
     }
     
     static boolean determineNumberNegative(int a) {
@@ -33,12 +33,7 @@ class HomeTask2 {
     System.out.println();
     }
     
-    static void determineLeapYear() {
-		int year = 1000;
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            System.out.println("Leap year - true.");
-        } else {
-            System.out.println("Leap year - false.");
+    static boolean determineLeapYear(int year) {
+		return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
         }
-    }
 }
